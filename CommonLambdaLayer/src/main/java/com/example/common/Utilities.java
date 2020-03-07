0103f1a6eceadcbf8726bcc074264666df395b37
Ionut <ionut.dejeu@gamail.com>
2020-03-07 14:48:01 +0200

    Initial Commit

---
+++ CommonLambdaLayer/src/main/java/com/example/common/Utilities.java
@@ -0,0 +1,19 @@
+package com.example.common;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Utilities {
+
+    public static void logMessage(LambdaLogger logger, String msg) {
+        logger.log("Message: " + msg.toString());
+        StringWriter sw = new StringWriter();
+        logger.log(sw.toString().replaceAll("\n", "\r"));
+    }
+    public static String GetMessage(){
+        return "Hello from Lambda Layer";
+    }
+
+}
---
+++ CommonLambdaLayer/src/main/java/common/CommonLibrary.java
@@ -0,0 +1,10 @@
+package common;
+
+public class CommonLibrary {
+    public CommonLibrary(){
+    }
+
+    public static String getMessage(){
+        return "message from common library";
+    }
+}
